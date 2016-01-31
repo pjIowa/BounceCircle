90efa9b163a253f11662db0b47233151d782cb99
pjIowa <pjkedilaya@gmail.com>
2016-01-30 22:24:02 -0700

    first commit

---
+++ CircleViews/BounceScene.swift
@@ -0,0 +1,13 @@
+//
+//  BounceScene.swift
+//  CircleViews
+//
+//  Created by Prajwal Kedilaya on 9/24/15.
+//  Copyright © 2015 Prajwal Kedilaya. All rights reserved.
+//
+
+import UIKit
+
+class BounceScene: SKScene {
+
+}
---
+++ CircleViews/SceneController.swift
@@ -0,0 +1,35 @@
+//
+//  SceneController.swift
+//  CircleViews
+//
+//  Created by Prajwal Kedilaya on 9/27/15.
+//  Copyright © 2015 Prajwal Kedilaya. All rights reserved.
+//
+
+import UIKit
+
+class SceneController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
